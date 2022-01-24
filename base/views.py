f4a679aee0d37064650d8ce0785cddedc6da062f
Rob <rob@example.com>
2022-01-24 20:43:22 +0300

    add mobile styles

--- base/views.py
+++ base/views.py
@@ -59,7 +59,7 @@ def registerPage(request):
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') is not None else ''
     rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -160,3 +160,17 @@ def deleteMessage(request, pk):
     context = {'obj': message}
 
     return render(request, 'base/delete.html', context)
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+
+    context = {'topics': topics}
+    return render(request, 'base/topics.html', context)
+
+
+def activitiesPage(request):
+    room_messages = Message.objects.all()
+
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
